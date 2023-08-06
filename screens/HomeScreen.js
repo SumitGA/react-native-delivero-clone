13ae9c7ce29bce261d7af729e1a0189c3c0b7596
Sumit Gautam <sghost33@gmail.com>
2023-08-06 18:18:35 +1000

    Setup home screen for delivero app

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-500">HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
