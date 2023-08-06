5658d82535640bbbac8aef0961196a719ffe60c5
Sumit Gautam <sghost33@gmail.com>
2023-08-06 18:30:21 +1000

    Setup header section for delivero app on react-native

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React, { useLayoutEffect } from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShow: false,
+    })
+  }, [])
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
   )
 }
 
